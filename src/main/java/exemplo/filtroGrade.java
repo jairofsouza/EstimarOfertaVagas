42803acea5e3d9f865bbb4ed975e3a24306e77af
Joao Souza <joao.souza@ice.ufjf.br>
2015-06-05 14:58:54 -0300

    Atualizado o exemplo de filtros

--- src/main/java/exemplo/filtroGrade.java
+++ src/main/java/exemplo/filtroGrade.java
@@ -9,34 +9,50 @@ import br.ufjf.coordenacao.OfertaVagas.estimate.EstimativesResult;
 import br.ufjf.coordenacao.OfertaVagas.estimate.Estimator;
 import br.ufjf.coordenacao.OfertaVagas.loader.CSVCurriculumLoader;
 import br.ufjf.coordenacao.OfertaVagas.loader.CSVStudentLoader;
-import br.ufjf.coordenacao.OfertaVagas.loader.CurriculumSemesterFilter;
+import br.ufjf.coordenacao.OfertaVagas.loader.CurriculumElectivesFilter;
+
+import br.ufjf.coordenacao.OfertaVagas.loader.StudentGradeFilter;
 import br.ufjf.coordenacao.OfertaVagas.model.Curriculum;
 import br.ufjf.coordenacao.OfertaVagas.model.StudentsHistory;
 import br.ufjf.coordenacao.OfertaVagas.report.HTMLDetailedReport;
 
+
 public class filtroGrade {
-	
+
 	/*
 	 * Classe de exemplo para testar o filtro de periodos.
 	 */
-	
+
 	public static void main(String args[]) throws IOException
 	{
-				CSVCurriculumLoader csvcur = new CSVCurriculumLoader(
-						new File("data/35A_grade_obrigatorias_2014.txt"),
-						new File("data/35A_eletivas_2014.txt"),
-						new File("data/35A_equivalencias.txt"), new CurriculumSemesterFilter("2"));
-				Curriculum c = csvcur.getCurriculum();
-				
-				CSVStudentLoader csv = new CSVStudentLoader(new File("data/35A_alunos_2014.csv"));
-				StudentsHistory sh = csv.getStudentsHistory();
 
-				Estimator estimator = new Estimator(c, sh);
-				EstimativesResult result = estimator.populateData().process(0.9f, 0.6f, 0.7f, 0.8f, 0.5f);
+		/*
+		 * O construtor da classe CSVCurriculumLoader usa o arquivo de obrigatorias, eletivas e equivalencias da disciplina como parametros obrigatorios. O filtro e adicionado
+		 * no proximo parametro. ƒ possivel utilizar qualquer CurriculumFilter nesse campo (CurriculumDisciplineFiler, CurriculumElectiveFilter, ...).
+		 * Se nao for passado nenhum filtro, o construtor usa automaticamente o NoFilter, onde nenhum filtro e aplicado.
+		 */
 
-				File file = new File("data/result/resultado35A_2009_"+Calendar.getInstance().getTimeInMillis()+".html");
-				file.createNewFile();
-				PrintStream ps = new PrintStream(file); 
-				new HTMLDetailedReport(ps).generate(result, sh, c);
+		CSVCurriculumLoader csvcur = new CSVCurriculumLoader(
+				new File("data/35A_grade_obrigatorias_2014.txt"),
+				new File("data/35A_eletivas_2014.txt"),
+				new File("data/35A_equivalencias.txt"), new CurriculumElectivesFilter());
+		Curriculum c = csvcur.getCurriculum();
+		
+		/*
+		 * A classe CSVCurriculumLoader e construido com o arquivo de alunos. Tambem e possivel usar qualquer StudentFilter (StudentGradeFilter, StudentIngressFilter)
+		 * como proximo parametro do arquivo. Se nenhum filtro for aplicado, a classe tambem utiliza o NoFilter automaticamente.
+		 */
+		
+		CSVStudentLoader csv = new CSVStudentLoader(new File("data/35A_alunos_2014.csv"), new StudentGradeFilter("12014"));
+		StudentsHistory sh = csv.getStudentsHistory();
+
+		Estimator estimator = new Estimator(c, sh);
+		EstimativesResult result = estimator.populateData().process(0.9f, 0.6f, 0.7f, 0.8f, 0.5f);
+
+		File file = new File("data/result/resultado35A_filtro_"+Calendar.getInstance().getTimeInMillis()+".html");
+		file.createNewFile();
+		PrintStream ps = new PrintStream(file); 
+		new HTMLDetailedReport(ps).generate(result, sh, c);
+		
 	}
 }
