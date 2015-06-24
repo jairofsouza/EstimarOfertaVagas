edcb7add764daf6b5a6d934f4e945be7fe7570ee
Joao Souza <joao.souza@ice.ufjf.br>
2015-06-24 15:01:13 -0300

    Adicionado calculo do IRA

--- src/main/java/br/ufjf/coordenacao/OfertaVagas/model/Student.java
+++ src/main/java/br/ufjf/coordenacao/OfertaVagas/model/Student.java
@@ -9,6 +9,7 @@ public class Student implements Comparable<Student>{
 	private String _id;
 	private String _nome;
 	private String _curriculum;
+	private int _firstSemester;
 	
 	public String getNome() {
 		return _nome;
@@ -28,11 +29,77 @@ public class Student implements Comparable<Student>{
 	
 	public Student(String id) {
 		this._id = id;
+		this._firstSemester = 0;
 	}
 	
 	public Student(String id, String nome) {
 		this._id = id;
 		this._nome = nome;
+		this._firstSemester = 0;
+	}
+	
+	public float getIRA() { return IRA(0, 99999); }
+	public float getIRA(int semester) { return IRA(this._firstSemester, semester); }
+	public float getSemesterIRA(int semester) { return IRA(semester, semester); }
+	
+	private float IRA(int firstSemester, int lastSemester)
+	{
+		HashMap<Class, ArrayList<String[]>> cl;
+		int grade = 0;
+		int weight = 0;
+
+		cl = this.classes.get(ClassStatus.APPROVED);
+		if(cl != null) for(Class c: cl.keySet())
+		{
+			ArrayList<String[]> classdata = cl.get(c);
+			for(String[] s2: classdata)
+			{
+				//TODO verificar excecoes do calculo - #3 (A a E)
+				if(s2[1].equals("APR") || s2[1].equals("DISP") || s2[1].equals("A"))
+					continue;
+
+				//Verifica se o semestre esta dentro do intervalo definido
+				if(Integer.valueOf(s2[0]) < firstSemester || Integer.valueOf(s2[0]) > lastSemester)
+					continue;
+
+				grade += Integer.valueOf(s2[1]) * c.getWeight();
+				weight += c.getWeight();
+			}
+		}
+
+		cl = this.classes.get(ClassStatus.REPROVED_GRADE);
+		if(cl != null) for(Class c:cl.keySet())
+		{
+			ArrayList<String[]> classdata = cl.get(c);
+			for(String[] s2: classdata)
+			{
+				if(s2[1].equals(""))
+					s2[1] = "0";
+				if(s2[1].equals("NC"))
+					continue;
+
+				if(Integer.valueOf(s2[0]) < firstSemester || Integer.valueOf(s2[0]) > lastSemester)
+					continue;
+
+				grade += Integer.valueOf(s2[1]) * c.getWeight();
+				weight += c.getWeight();
+			}
+		}
+
+		cl = this.classes.get(ClassStatus.REPROVED_FREQUENCY);
+		if(cl != null) for(Class c: cl.keySet())
+		{	
+			ArrayList<String[]> classdata = cl.get(c);
+			for(String[] s2: classdata)
+			{
+				if(Integer.valueOf(s2[0]) < firstSemester || Integer.valueOf(s2[0]) > lastSemester)
+					continue;
+
+				weight += c.getWeight();
+			}
+
+		}
+		return (float) grade / weight;
 	}
 	
 	public void addClass(String _class, String semester, ClassStatus status, String grade, String weight) {
@@ -51,15 +118,22 @@ public class Student implements Comparable<Student>{
 		//Adiciona os creditos da disciplina
 		_class2.setWeight(Integer.valueOf(weight));
 		
-		String[] o = new String[2];
+		String[] o = new String[2]; //Array que guarda as informacoes de cada vez que o aluno fez a disciplina (Semestre, Disciplina...)
 		
 		o[0] = semester;//adiciona o semestre cursado
 		o[1] = grade; //adiciona a nota da disciplina
 		a.add(o);
 		this.classes.get(status).put(_class2, a);
+		
+		int sem = Integer.valueOf(semester);
+		
+		if(sem < this._firstSemester || this._firstSemester == 0)
+			this._firstSemester = sem;
 	}
 	
 	public String getId() { return this._id; }
+	
+	public int getFirstSemester() { return this._firstSemester; }
 	
 	@Override
 	public String toString() {
@@ -80,7 +154,7 @@ public class Student implements Comparable<Student>{
 
 		return classes.get(cs);
 	}
-
+		
 	@Override
 	public int compareTo(Student s)
 	{
---
+++ src/main/java/exemplo/CalculoIRA.java
@@ -0,0 +1,31 @@
+package exemplo;
+
+import java.io.File;
+import java.io.IOException;
+
+import br.ufjf.coordenacao.OfertaVagas.loader.CSVStudentLoader;
+import br.ufjf.coordenacao.OfertaVagas.model.Student;
+import br.ufjf.coordenacao.OfertaVagas.model.StudentsHistory;
+
+public class CalculoIRA {
+
+	/*
+	 * Classe de exemplo que mostra como obter o IRA de um aluno.
+	 * ƒ possivel obter o IRA total do aluno, o de um semestre especifico ou
+	 * o de um intervalo especifico
+	 */
+	public static void main(String[] args) throws IOException
+	{
+		//Carregando o historico de alunos
+		CSVStudentLoader csv = new CSVStudentLoader(new File("data/35A_alunos_2014.csv"));
+		StudentsHistory sh = csv.getStudentsHistory();
+		
+		//Selecionando um aluno qualquer
+		Student st = sh.getStudents().get("268580030,7");
+		
+		float ira1 = st.getIRA(); //Obtem o IRA total do aluno
+		float ira2 = st.getIRA(20131); //Obtem o IRA do aluno em um semestre (todas as disciplinas cursadas do 1o ate o semestre informado) (20131)
+		float ira3 = st.getSemesterIRA(20103); //Obtem o IRA do aluno com apenas as disciplinas que ele cursou naquele semestre (20103)
+		System.out.println("ALUNO: " + st.getId() + "\n-IRA total:" + ira1 + "\t-IRA em 20131: " + ira2 + "\t-IRA das disc. de 20103: " + ira3);
+	}
+}
